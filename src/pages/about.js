319ab8cf42ebbd11febe0cbef2af8ab9f6da2d41
Luna Graysen <luna.g@protonmail.com>
2020-04-08 14:09:41 -0400

    Add beginning of cityscapes content

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,11 +1,13 @@
 import React from "react"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
 
 const AboutPage = () => (
   <Layout>
-    <p>About</p>
+    <h4>About</h4>
+    <p>Luna Graysen is a 21 year old non-binary photographer and software engineer. They live in the Plateau Mont-Royal neighbourhood of Montreal with their partner Milo and kitty cat Herbie. They enjoy capturing street photography and have started dabbling into portraiture and product photography as well.</p>
+    <p>Luna uses a Sony A7 III camera body and a number of SIGMA ART lenses to capture their photographs.</p>
+    <p>Luna is self-represented and takes clients on a limited basis. Inquire about their availability:</p>
   </Layout>
 )
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,46 @@
 import React from "react"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
+
+export const Cityscapes = [
+  {
+    id: 1,
+    name: "2019, Frenco Épicerie.",
+    src: "./cityscapes-1.jpg"
+  },
+  {
+    id: 2,
+    name: "2019, Rue Bullion.",
+    src: "./cityscapes-2.jpg"
+  },
+  {
+    id: 3,
+    name: "2018, Somewhere in Vieux-Port.",
+    src: "./cityscapes-3.jpg"
+  },
+  {
+    id: 4,
+    name: "2018, Silo #5.",
+    src: "./cityscapes-4.jpg"
+  },
+  {
+    id: 5,
+    name: "2017, Rue Prince apartment.",
+    src: "./cityscapes-5.jpg"
+  },
+  {
+    id: 6,
+    name: "2017, Rue Prince apartment.",
+    src: "./cityscapes-6.jpg"
+  }
+];
 
 const IndexPage = () => (
   <Layout>
-    <p>Cityscapes</p>
+    {Cityscapes.map(image => {
+      console.log(image.src);
+      return <img key={image.id} src={image.src} alt="can't show image" />;
+    })}
   </Layout>
 )
 
